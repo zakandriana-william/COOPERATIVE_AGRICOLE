35ddb8430d17fef040927d247fb921874732b437
zakandriana-william <wzakandriana@gmail.com>
2026-04-26 12:04:49 +0300

    Update axios.js

--- agricoop-frontend/src/api/axios.js
+++ agricoop-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',          // proxy vers http://localhost:5000/api
+  baseURL: 'cooperative-agricole-n8ly.vercel.app',          // proxy vers http://localhost:5000/api
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
