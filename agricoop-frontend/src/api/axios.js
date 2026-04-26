5c1ca2256156fd3c3d85d4dfb7c3f876ec23f235
zakandriana-william <wzakandriana@gmail.com>
2026-04-26 13:31:07 +0300

    Update axios.js

--- agricoop-frontend/src/api/axios.js
+++ agricoop-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://cooperative-agricole.onrender.com/api',          // proxy vers http://localhost:5000/api
+  baseURL: 'https://cooperative-agricole.onrender.com/api',         // proxy vers http://localhost:5000/api
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
