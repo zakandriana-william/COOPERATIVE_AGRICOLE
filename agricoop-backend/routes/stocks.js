a080a1eeaab15b41000e10a3a56e4fb4901b835b
zakandriana-william <wzakandriana@gmail.com>
2026-04-28 11:07:18 +0300

    Update stocks.js

--- agricoop-backend/routes/stocks.js
+++ agricoop-backend/routes/stocks.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router  = express.Router()
 const ctrl    = require('../controllers/stocksController')
-const { protect, adminOrGest } = require('../middleware/auth')
+const { protect, adminOrGest, adminOnly } = require('../middleware/auth')
 
 router.use(protect)
-router.get ('/produits/alertes', adminOrGest, ctrl.getAlertes)
-router.get ('/produits',         adminOrGest, ctrl.getProduits)
-router.get ('/produits/:id',     adminOrGest, ctrl.getProduitById)
-router.post('/produits',         adminOrGest, ctrl.createProduit)
-router.put ('/produits/:id',     adminOrGest, ctrl.updateProduit)
-router.get ('/mouvements',       adminOrGest, ctrl.getMouvements)
-router.post('/mouvements',       adminOrGest, ctrl.createMouvement)
+router.get('/alertes',       adminOrGest, ctrl.getAlertes)
+router.get('/produits',      adminOrGest, ctrl.getProduits)
+router.get('/produits/:id',  adminOrGest, ctrl.getProduitById)
+router.post('/produits',     adminOnly,   ctrl.createProduit)
+router.put('/produits/:id',  adminOnly,   ctrl.updateProduit)
+router.get('/mouvements',    adminOrGest, ctrl.getMouvements)
+router.post('/mouvements',   adminOrGest, ctrl.createMouvement)
 
 module.exports = router
